5616c7b845aa0925fe454eac9f1a4f78389e097b
Jennifer <jennifer15aries@gmail.con>
2024-05-17 23:15:48 -0500

    Cambio 1

--- Guitarras/src/App.jsx
+++ Guitarras/src/App.jsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState , useEffect } from 'react'
+import  Header  from "./components/Header"
+import Guitar from "./components/Guitar"
+import { db } from './data/db'
 
 function App() {
-  const [count, setCount] = useState(0)
+  /*data contendrá el estado actual, mientras que setData será una función que se utilizará para actualizar el estado data. */
+  const [data,setData] = useState(db)
+  /*Se inicializa con un array vacío */
+  const [cart,setCart] = useState([])
 
+  /* agregar elementos al carrito */
+  function addToCart(item){
+    
+    const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+
+    if(itemExists>=0){//si existe el carrito
+      //Es inmutable pq primero crea una copia
+      const updatedCart = [...cart]
+      updatedCart[itemExists].quantity++
+      setCart(updatedCart)
+    }else{
+      item.quantity = 1
+      /* toma el estado anterior (prevCart) y agrega el nuevo elemento (item) al final del array */
+      setCart([...cart,item])
+    }
+    
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hola musdo desde </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <Header
+      cart={cart}
+    />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar
+            /*identificar qué elementos han cambiado, se añadido o eliminado */
+              key={guitar.id}
+              /*Se crea el props de guitar */
+              guitar={guitar}
+              /*permitir que el componente Guitar actualice el estado del carrito. */
+              setCart={setCart}
+              /*permitir que el componente Guitar agregue elementos al carrito. */
+              addToCart={addToCart}
+            />
+          ))}
+            
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+      </footer>
     </>
   )
 }
